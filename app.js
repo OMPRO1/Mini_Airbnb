b9572cb82b674202e4bf83404ad2746ef28cf91b
Ruturaj Suresh Tarapurkar <ruturajtarapurkar100@gmail.com>
2024-02-09 13:27:44 +0530

    Added styling

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const mongoose = require('mongoose');
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const Listing = require("./models/Listing.js");
 const path = require('path');
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"public")));
 
 async function main(){
     await mongoose.connect(MONGO_URL);
@@ -46,12 +52,28 @@ app.get("/listings/:id", async (req,res) => {
     res.render("./listings/show.ejs",{listing});
 }); 
   
-app.post("/listings", (req,res) =>{
-    let data = req.body;
-     console.log(data);
-     res.redirect("/listings");
+app.post("/listings", async (req,res) =>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
 });
 
-app.get("/listings/show" , (req,res) => {
-    res.send("Working!");
+app.get("/listings/:id/edit" , async (req,res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+
+    res.render("./listings/edit.ejs",{listing});
+});
+
+app.put("/listings/:id" , async (req,res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect("/listings");
+});
+
+app.delete("/listings/:id", async (req,res) => {
+    let {id} = req.params;
+    const deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
 });
