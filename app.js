6b1bedf6fd828fa41320ff4350d146abf02b83a1
Ruturaj Suresh Tarapurkar <ruturajtarapurkar100@gmail.com>
2024-02-16 19:04:01 +0530

    Added express Router

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const wrapAsync = require('./utils/wrapAsync.js');
 const ExpressError = require('./utils/ExpressError.js');
 const {listingSchema , reviewSchema} = require('./schema.js');
 const Review = require("./models/review.js");
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
+
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -29,15 +32,6 @@ main()
 .catch((err) => { 
     console.log(err);
 });
-
-const validateListing = (req,res,next) => {
-    let {error} = listingSchema.validate(req.body);
-    if(error){
-        throw new ExpressError(400,error);
-    }else{
-        next()
-    }
-}
 
 
 const validateReview = (req,res,next) => {
@@ -59,77 +53,8 @@ app.get("/" ,(req,res) => {
     res.send("Working");
 });
 
-app.get("/listings", wrapAsync(async (req,res) => {
-    const allListings = await Listing.find({});
-    //console.log(allListings);
-  
-    res.render("./listings/index.ejs",{allListings});
-}));
-
-
-app.get("/listings/new" , (req,res) => {
-    res.render("./listings/new.ejs");
-}); 
-
-app.get("/listings/:id" , wrapAsync(async (req,res,next) => {
-    let {id} = req.params;
-    console.log(id);
-    const listing = await Listing.findById(id).populate('reviews');
-    console.log(listing);
-    res.render("./listings/show.ejs",{listing});
-}));
-
-app.post("/listings",validateListing, wrapAsync(async (req,res,next) =>{
-    const newListing = new Listing(req.body.listing);
-    //console.log(newListing);
-    await newListing.save();
-    res.redirect("/listings");
-}));
-
-app.get("/listings/:id/edit" , wrapAsync(async (req,res,next) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("./listings/edit.ejs",{listing});
-}));
-
-app.put("/listings/:id" , validateListing ,wrapAsync(async (req,res) => {
-    if(!req.body.listing){
-        throw new ExpressError(400,"BAD REQUEST FROM CLIENT");
-    }
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect(`/listings/${id}`);
-}));
-
-app.delete("/listings/:id", wrapAsync(async (req,res) => {
-    let {id} = req.params;
-    const deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-}));
-
-//Review section 
-
-app.post("/listings/:id/reviews",validateReview, wrapAsync(async (req,res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = await new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-    await newReview.save();
-    await listing.save();
-
-    res.redirect(`/listings/${listing._id}`);
-}));
-
-// delete review route
-app.delete("/listings/:id/reviews/:reviewId", wrapAsync( async (req,res) => {
-    let {id,reviewId} = req.params;
-
-    await Review.findByIdAndDelete(reviewId);
-    await Listing.findByIdAndUpdate(id,{$pull: {reviews : reviewId}});
-
-    res.redirect(`/listings/${id}`);
-}));
+app.use("/listings",listings);
+app.use("/listings/:id/reviews",reviews);
 
 //If a request comes on random route then error should be throwm
 // app.all("*", (req,res,next) => {
