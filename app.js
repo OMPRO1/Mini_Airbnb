64225c61255d28a4140c54102a3be36271f8db26
Ruturaj Suresh Tarapurkar <ruturajtarapurkar100@gmail.com>
2024-02-17 14:11:48 +0530

    Added Middlewares

--- app.js
+++ app.js
@@ -31,19 +31,7 @@ main()
 })
 .catch((err) => { 
     console.log(err);
-});
-
-
-const validateReview = (req,res,next) => {
-    let {error} = reviewSchema.validate(req.body);
-    if(error){
-        throw new ExpressError(400,error);
-    }else{
-        next();
-    }
-}
-
-  
+});  
 
 app.listen(8080, () => {
     console.log("Server running on port:8080");
