459ed58f6e8784276b892e8e30ce14dc507ce0f3
Ruturaj Suresh Tarapurkar <ruturajtarapurkar100@gmail.com>
2024-02-24 15:58:59 +0530

    Added atlas connections

--- init/index.js
+++ init/index.js
@@ -2,23 +2,22 @@ const mongoose = require('mongoose');
 const initData = require("./data.js");
 const Listing = require("../models/Listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-
-async function main(){
-    await mongoose.connect(MONGO_URL);
-}
-
-main()
-.then(() => {
-    console.log("Connected");
-})
-.catch((err) => {
-    console.log(err);
-});
+const dbUrl = "mongodb+srv://ruturajstarapurkar:j4PRsrX0yDmw3pFM@cluster0.il2vodz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  main()
+    .then(() => {
+      console.log("connected to DB");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  
+  async function main() {
+    await mongoose.connect(dbUrl);
+  }
 
 const initDB = async () => {
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({...obj, owner: "65d73af0659bfe682a7c07ba"}));
+    initData.data = initData.data.map((obj) => ({...obj, owner: "65d9bfe331f1313f0c6ac904"}));
     await Listing.insertMany(initData.data);
     console.log("Data added");
 }
--- models/Listing.js
+++ models/Listing.js
@@ -27,11 +27,9 @@ const listingSchema = new Schema({
     type: {
       type: String, // Don't do `{ location: { type: String } }`
       enum: ['Point'], // 'location.type' must be 'Point'
-      required: true
     },
     coordinates: {
       type: [Number],
-      required: true
     },
   },
 },
